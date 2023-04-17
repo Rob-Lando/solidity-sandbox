86e30c54fac04e73851f49828056c3052649a97c
Rob-Lando <Roblando95@outlook.com>
2023-04-17 01:26:14 -0400

    hello world contract

---
+++ projects/contracts/hello_world.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+
+contract ay_yo_whudup {
+    
+    // define state variables
+    string greeting;
+
+    function set_greeting(string memory _dude) public {
+
+            greeting = string.concat("Ay yo whudup, ",_dude,"!!!");
+
+    }
+
+    function greet() public view returns (string memory) {
+        
+        return greeting;
+    
+    }
+
+}
