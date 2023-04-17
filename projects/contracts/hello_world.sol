037c5a7b9e18d45a72075c80208ed369c15935b4
Rob-Lando <Roblando95@outlook.com>
2023-04-17 02:28:15 -0400

    modify contract to append message details to a running array

--- projects/contracts/hello_world.sol
+++ projects/contracts/hello_world.sol
@@ -3,18 +3,53 @@ pragma solidity ^0.8.0;
 
 contract ay_yo_whudup {
     
+    struct message_details {
+        uint num;
+        address sender;
+        string to;
+        string message;
+    
+    }
+
     // define state variables
+    uint num_greets;
     string greeting;
+    //mapping(uint => message_details) public greetings;
+    message_details[] greetings;
 
     function set_greeting(string memory _dude) public {
 
             greeting = string.concat("Ay yo whudup, ",_dude,"!!!");
+            
+            greetings.push(
+                            message_details(
+                                        {
+                                            num: num_greets,
+                                            sender:     msg.sender,
+                                            to:         _dude,
+                                            message:    greeting
+                                        }
+                                    )
+            );
+
+            /*
+            greetings[num_greets] = message_details({
+                                        sender:     msg.sender,
+                                        to:         _dude,
+                                        message:    greeting
+                                        });
+            */
+            num_greets++;
 
     }
 
-    function greet() public view returns (string memory) {
-        
+    function last_greet() public view returns (string memory) {
         return greeting;
+    }
+
+    function all_greetings() public view returns (message_details[] memory) {
+        
+        return greetings;
     
     }
 
