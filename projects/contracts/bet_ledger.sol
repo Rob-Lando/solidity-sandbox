3c8cb2e2326b25fa0550a9ffd5f3d72f6c7f917d
Rob-Lando <Roblando95@outlook.com>
2023-04-19 02:50:54 -0400

    update to include ETH/USD Sepolia price feed Oracle

--- projects/contracts/bet_ledger.sol
+++ projects/contracts/bet_ledger.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import "node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract price_prediction_market {
 
     receive() external payable {
-        // to funding contract for test payouts
+        // to fund contract for test payouts
+    }
+
+    AggregatorV3Interface public price_oracle;
+    constructor() {
+        price_oracle = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
     }
 
     enum Status {open,closed}
@@ -12,13 +19,31 @@ contract price_prediction_market {
     struct prediction_details {
         //uint    unix_timestamp;
         string  symbol;
+        int    reference_price;
         uint    direction; // 0 = down, 1 = up
         uint    wager;
         Status  status;
     }
 
-    //set state variables
+    // set state variables
     mapping(address => prediction_details[]) history;
+    int eth_usd;
+
+    function set_ETHUSD_latest_price() public returns (int) {
+
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = price_oracle.latestRoundData();
+
+        eth_usd = price;
+
+        return eth_usd;
+
+    }
 
     function set_prediction_details(string memory _symbol, uint _direction, uint _wager) public returns (prediction_details memory) {
 
@@ -28,9 +53,10 @@ contract price_prediction_market {
                                                         {
                                                             // unix_timestamp:_unix_timestamp,
                                                             // eventually this will be part of the struct, need to make an api call to get a reliable stamp
-                                                            symbol:_symbol,
-                                                            direction:_direction,
-                                                            wager:_wager,
+                                                            symbol: _symbol,
+                                                            reference_price: eth_usd,
+                                                            direction: _direction,
+                                                            wager: _wager,
                                                             status: Status.open
                                                         }
                                                     );
@@ -45,6 +71,5 @@ contract price_prediction_market {
         return history[msg.sender];
 
     }
-
 
 }
