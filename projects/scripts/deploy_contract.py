7eb892b4618ba9bc964b461af5e8d0f7ae5ce13e
Rob-Lando <Roblando95@outlook.com>
2023-05-08 01:29:20 -0400

    load env vars first, decrypt later

--- projects/scripts/deploy_contract.py
+++ projects/scripts/deploy_contract.py
@@ -1,19 +1,21 @@
 import shutil
+import os
 from web3 import Web3
 from modules import env_encryptor
 
 
+def get_env_vars(path_to_env_file: str, env_var_names: list) -> dict:
 
-def main(path_to_env_file: str, env_var_names: list):
+    env_encryptor.load_env(path_to_env_file)
 
-    env_encryptor.load_env("../.env")
+    env_vars = {var:os.environ[var] for var in env_var_names}
 
-    _env_vars = {var:env_encryptor.decrypt_env_secret(secret_name = var) for var in env_var_names}
+    return env_vars
 
 
 if __name__ == "__main__":
     
-    main(path_to_env_file = "../.env", env_var_names = ["INFURA_API_KEY"])
+    get_env_vars(path_to_env_file = "../.env", env_var_names = ["INFURA_API_KEY"])
 
     # clear module cache so latest version is always imported
     module_cache_path = "/".join(env_encryptor.__file__.replace("env_encryptor.py","__pycache__").split("\\")[-2:])
