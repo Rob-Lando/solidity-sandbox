e5dae5e210672e2eb2fff8029e4904db07ddef6d
Rob-Lando <Roblando95@outlook.com>
2023-05-13 02:48:46 -0400

    read source .sol file and compile with solc

--- projects/scripts/deploy_contract.py
+++ projects/scripts/deploy_contract.py
@@ -1,9 +1,15 @@
 from web3 import Web3
+from solcx import (
+    install_solc, 
+    compile_source
+)
 from modules.env_encryptor import (
     get_env_vars,
     key_gen,
     decrypt_env_secret
 )
+
+def compile_sol(path,solc_version)
 
 if __name__ == "__main__":
     
@@ -14,7 +20,21 @@ if __name__ == "__main__":
 
     infura_url = f"https://sepolia.infura.io/v3/{decrypt_env_secret(_bin_key = _key, secret_name = 'INFURA_API_KEY')}"
 
-    web3 = Web3(Web3.HTTPProvider(infura_url))
+    w3 = Web3(Web3.HTTPProvider(infura_url))
 
-    print(web3.isConnected())
+    print(w3.isConnected())
+
+    install_solc(version = "0.8.0")
+    with open('../contracts/bet_ledger.sol', 'r') as sol:
+        source = sol.read()
+
+    compiled_src = compile_source(source,
+                                 output_values=["abi", "bin-runtime"],
+                                 solc_version="0.8.0")
+    
+    compiled_src = compiled_src[list(compiled_src.keys())[0]]
+
+    print(compiled_src['abi'],"\n\n",compiled_src['bin-runtime'])
+
+    #contract = w3.eth.contract(abi = compiled_src["abi"], bytecode = compiled_src["bin-runtime"])
 
